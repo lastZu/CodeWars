8c2c6e58caf99193edf84b581bc5c64fb7d5e513
lastKraken <cdrom2015@ya.ru>
2023-03-03 04:36:21 +0300

    1

--- main.go
+++ main.go
@@ -4,15 +4,9 @@ import "fmt"
 
 func main() {
 	// DigitalRoot
-	fmt.Println(DigitalRoot(5))
-	fmt.Println(DigitalRoot(15))
-	fmt.Println(DigitalRoot(55))
-	fmt.Println(DigitalRoot(125))
-	fmt.Println(DigitalRoot(555))
-
-	fmt.Println(DigitalRoot2(5))
-	fmt.Println(DigitalRoot2(15))
-	fmt.Println(DigitalRoot2(55))
-	fmt.Println(DigitalRoot2(125))
-	fmt.Println(DigitalRoot2(555))
+	fmt.Println(FormatDuration(5))
+	fmt.Println(FormatDuration(15))
+	fmt.Println(FormatDuration(111111))
+	fmt.Println(FormatDuration(3603))
+	fmt.Println(FormatDuration(3601))
 }
